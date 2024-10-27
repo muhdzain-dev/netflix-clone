b68a28b0964a71dac881d816b85000937fb20cc4
muhdzain-dev <muhd.zain.dev@gmail.com>
2024-10-27 21:09:09 +0400

    Done with Login page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/login";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 };
