7ddc84ef678ef95b93bd45b44834206747bc38c5
muhdzain-dev <muhd.zain.dev@gmail.com>
2024-10-27 19:03:00 +0400

    Complete Design of Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import searchIcon from "../../assets/search_icon.svg";
+import bellIcon from "../../assets/bell_icon.svg";
+import profileIcon from "../../assets/profile_img.png";
+import caretIcon from "../../assets/caret_icon.svg";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="logo" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Language</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={searchIcon} alt="searchIcon" className="icons" />
+        <p>Children</p>
+        <img src={bellIcon} alt="bellIcon" className="icons" />
+        <div className="navbar-profile">
+          <img src={profileIcon} alt="profileIcon" className="profile" />
+          <img src={caretIcon} alt="caretIcon" />
+          <div className="dropdown">
+            <p>Sign Out</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      Home
     </div>
   );
 };
