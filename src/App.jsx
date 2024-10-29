1b82713c7aa123aa69205109b08451ea5f965ebd
Muhammad Zain <muhd.zain.dev@gmail.com>
2024-10-29 23:36:05 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/Home/Home";
-import Login from "./pages/Login/login";
+import Login from "./pages/Login/Login";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
